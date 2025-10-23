e67fe69d08a8b51b0ad6c3bb8a46557839170383
Ed Brooks <brookse@me.com>
2025-10-23 12:05:31 +1100

    stripe: singleton + verify

---
+++ frontend/src/lib/stripe.ts
@@ -0,0 +1,8 @@
+import Stripe from 'stripe';
+import { ENV } from '@/lib/env';
+
+export const stripe = new Stripe(ENV.server.STRIPE_SECRET_KEY, { apiVersion: '2025-09-30.clover' });
+
+export function verifyWebhook(raw: Buffer, sig: string) {
+  return stripe.webhooks.constructEvent(raw, sig, ENV.server.STRIPE_WEBHOOK_SECRET);
+}
